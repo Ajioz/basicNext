ddcbd4cf4381a6115285a59ac34fe823848cbc23
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-07-24 16:26:07 +0100

    added mainHeader

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,18 @@
-import './globals.css'
+import "./globals.css";
+import Header from "@/components/mainHeader/Header";
 
 export const metadata = {
-  title: 'NextJS Course App',
-  description: 'Your first NextJS app!',
+  title: "News",
+  description: "Modern News app!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- components/mainHeader/Header.jsx
+++ components/mainHeader/Header.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import style from "./header.module.css";
 import HeaderBackground from "./headerBackground";
+import logoImg from "@/assets/logo.png";
+import Link from "next/link";
+import NavLink from "./navLink";
+import Image from "next/image";
 
 const Header = () => {
   return (
@@ -10,11 +14,17 @@ const Header = () => {
         {" "}
         <Link href="/" className={style.logo}>
           <Image src={logoImg} alt="logo" priority />
-          News App
+          News
         </Link>
         <nav className={style.nav}>
           <ul>
-            <li></li>
+            <li>
+              <NavLink href="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink href='/news'>New Link</NavLink>
+              {/* <NavLink href={`/${id}`}>{id}</NavLink> */}
+            </li>
           </ul>
         </nav>
       </header>
