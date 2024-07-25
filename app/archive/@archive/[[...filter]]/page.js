b35054cced458ca35d6435a7a23c54e9c057bf51
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-07-25 15:28:35 +0100

    catched errors

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,20 +1,34 @@
 import React from "react";
 import NewsList from "@/components/newsList/NewsList";
-import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
 import Link from "next/link";
 
 const numToMonth = {
-  1: "Jan", 2: "Feb", 3: "Mar", 4:"Apr", 5:"May", 6:"Jun", 7:"Jul", 8:"Aug", 9:"Sep", 10:"Oct", 11:"Nov", 12:"Dec"
-}
-
+  1: "Jan",
+  2: "Feb",
+  3: "Mar",
+  4: "Apr",
+  5: "May",
+  6: "Jun",
+  7: "Jul",
+  8: "Aug",
+  9: "Sep",
+  10: "Oct",
+  11: "Nov",
+  12: "Dec",
+};
 
 const FilteredNewsPage = ({ params }) => {
   const filter = params.filter;
-  
+
   const selectedYear = filter?.[0];
   const selectedMonth = filter?.[1];
-  // const selectedDay = filter?.[2];
-  
+
   let news;
   let links = getAvailableNewsYears();
 
@@ -33,13 +47,23 @@ const FilteredNewsPage = ({ params }) => {
     newsContent = <NewsList news={news} />;
   }
 
+  if (
+    (selectedYear && !getAvailableNewsYears().includes(Number(selectedYear))) ||
+    (selectedMonth &&
+      !getAvailableNewsMonths(selectedYear).includes(Number(selectedMonth)))
+  ) {
+    throw new Error('Invalid filter!');
+  }
+
   return (
     <>
       <header id="archive-header">
         <nav>
           <ul>
             {links.map((link) => {
-              const href = selectedYear ? `/archive/${selectedYear}/${link}` : `/archive/${link}`
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
               return (
                 <li key={link}>
                   <Link href={href}>
