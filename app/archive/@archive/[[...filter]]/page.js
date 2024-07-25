fffed743b89e90f15bdb824574806de1b786290c
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-07-25 15:01:15 +0100

    improved filter

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,59 @@
+import React from "react";
+import NewsList from "@/components/newsList/NewsList";
+import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
+import Link from "next/link";
+
+const numToMonth = {
+  1: "Jan", 2: "Feb", 3: "Mar", 4:"Apr", 5:"May", 6:"Jun", 7:"Jul", 8:"Aug", 9:"Sep", 10:"Oct", 11:"Nov", 12:"Dec"
+}
+
+
+const FilteredNewsPage = ({ params }) => {
+  const filter = params.filter;
+  
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+  // const selectedDay = filter?.[2];
+  
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>Make a valid selection period to see news feed.</p>;
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear ? `/archive/${selectedYear}/${link}` : `/archive/${link}`
+              return (
+                <li key={link}>
+                  <Link href={href}>
+                    {!selectedYear ? link : numToMonth[link]}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
+};
+
+export default FilteredNewsPage;
