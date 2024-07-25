6a8127a2a0ca6acca7f3a84df738986e26b4ade4
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-07-25 10:06:32 +0100

    detail page finished

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import { DUMMY_NEWS } from "@/dummy-news";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import React from "react";
 
-const NewsDetails = ({params}) => {
+const NewsDetails = ({ params }) => {
+  const newSlug = params.slug;
+  const Item = DUMMY_NEWS.find(
+    (item) => item.slug === newSlug
+  );
+
+  if (!Item) notFound();
   return (
-    <article>
-      <h1>News Detail Page </h1>
-      <main>
-        <p>{params.id}</p>
-      </main>
+    <article className="news-article">
+      <header>
+        <Image
+          src={`/images/news/${Item.image}`}
+          title={Item.title}
+          width={100}
+          height={100}
+        />
+        <h1>{Item.title} </h1>
+        <time dateTime={Item.date}>{Item.date}</time>
+      </header>
+      <p>{Item.content}</p>
     </article>
   );
-}
+};
 
 export default NewsDetails;
