ea2d802a41d299e4e09975e4fbe584acecf1eff4
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-07-25 21:18:51 +0100

    navigating programatically

--- app/news/[slug]/@modal/(.)imagePage/page.js
+++ app/news/[slug]/@modal/(.)imagePage/page.js
@@ -1,15 +1,19 @@
+"use client";
+
 import { DUMMY_NEWS } from "@/dummy-news";
-import { notFound } from "next/navigation";
+import { notFound, useRouter } from "next/navigation";
 
 const InterceptedImagePage = ({ params }) => {
+  const router = useRouter();
   const imageSlug = params.slug;
   const Item = DUMMY_NEWS.find((item) => item.slug === imageSlug);
 
   if (!Item) notFound();
 
+
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${Item.image}`} alt={Item.title} />
