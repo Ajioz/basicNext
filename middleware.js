8f572ddb4272bb58a0c9f71f12cd45fe68137284
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-07-26 11:25:03 +0100

    added backend

---
+++ middleware.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+//A middleware to intercept some sever request, then grant or reject 
+export function middleware(request) {
+    console.log(request);
+    return NextResponse.next();
+}
+
+
+// A config file that tell which activity triggers the middleware
+export const config = {
+    matcher: '/news'
+}
